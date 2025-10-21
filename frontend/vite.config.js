14d8633b80280d715d4aef3cf15b5a9ad5cd52d6
KopatychDisko <95695789+KopatychDisko@users.noreply.github.com>
2025-10-21 12:52:27 +0300

    Done

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 80,
+    port: 8080,
     host: '127.0.0.1',
     strictPort: true
   },
